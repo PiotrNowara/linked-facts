68d6d1a239e40bc9ce1c65254391ee1350254182
PiotrNowara <piotrnowara@wp.pl>
2019-05-14 22:06:58 +0200

    clojars build config and core.clj refactoring

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject linked-facts "0.1.0-SNAPSHOT"
-  :description "Linked Facts"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 ;[tesser.core "1.0.3"] [clojure-csv/clojure-csv "2.0.1"] [clj-time "0.14.0"] [org.clojure/data.csv "0.1.4"]
-                 ]
+  :description "A lightweight library for analyzing events relationships"
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options" "-Xmx6g"]
+  :url "https://github.com/PiotrNowara/linked-facts"
+  :license "Eclipse Public License 1.0"
   ;:aot [linked-facts.core]
   ;:main linked-facts.core
   )
