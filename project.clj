f29a7b8396060822ba00eec3ef6816be161b8343
PiotrNowara <piotrnowara@wp.pl>
2019-05-14 22:07:48 +0200

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject linked-facts "0.1.0-SNAPSHOT"
   :description "A lightweight library for analyzing events relationships"
   :dependencies [[org.clojure/clojure "1.8.0"]]
-  :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options" "-Xmx6g"]
+  :javac-options ["-target" "1.6" "-source" "1.6" "-Xlint:-options" "-Xmx1g"]
   :url "https://github.com/PiotrNowara/linked-facts"
   :license "Eclipse Public License 1.0"
   ;:aot [linked-facts.core]
