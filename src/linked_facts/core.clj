48de97640c4a068021d2b4b4dc66f9632d0aaaa8
PiotrNowara <piotr.nowara@cdq.ch>
2022-05-02 23:19:25 +0200

    v0.2

--- src/linked_facts/core.clj
+++ src/linked_facts/core.clj
@@ -1,18 +1,30 @@
 (ns linked-facts.core
- (:require [clojure.core.reducers :as r])
-)
+ (:require [clojure.core.reducers :as r]))
 
-; 2019-05-14 New version with java.time instead joda-time/clj-time
-
+;;;04.2022 - v. 0.2 - bugfixes, tdiff cache, cleanup
 
 ;;;; UTILS
 (defn calculate-temporal-diff [d1 d2 chrono-unit] (.until d2 d1 chrono-unit))
 
-; This is the recommended function because they assume the date is already a date object (does not need to parse it over and over again!).
+
 (defn tdiff
   ([event1 event2] (tdiff event1 event2 :date java.time.temporal.ChronoUnit/DAYS))
   ([event1 event2 chrono-unit] (tdiff event1 event2 :date chrono-unit))
   ([event1 event2 date-key chrono-unit] (calculate-temporal-diff (get event1 date-key) (get event2 date-key) chrono-unit)))
+
+(def date-diffs (atom {}))
+(defn tdiff-cached
+  ([event1 event2] (tdiff-cached event1 event2 :date java.time.temporal.ChronoUnit/DAYS))
+  ([event1 event2 chrono-unit] (tdiff-cached event1 event2 :date chrono-unit))
+  ([event1 event2 date-key chrono-unit] (let [ev1-key (get event1 date-key)
+                                              ev2-key (get event2 date-key)
+                                              diff-key (str ev1-key ev2-key)
+                                              diff-res (get @date-diffs diff-key)]
+                                          (if (nil? diff-res)
+                                            (let [new-diff (calculate-temporal-diff ev1-key ev2-key chrono-unit)]
+                                              (swap! date-diffs assoc diff-key new-diff)
+                                              new-diff)
+                                            diff-res))))
 
 (defn tdiff-str
   ([event1 event2] (tdiff-str event1 event2 :date java.time.temporal.ChronoUnit/DAYS))
@@ -20,11 +32,7 @@
   ([event1 event2 date-key chrono-unit] (tdiff (java.time.LocalDate/parse (get event1 date-key)) (java.time.LocalDate/parse (get event2 date-key)) chrono-unit)))
 
 
-
 ;;;; MAIN FNs
-
-
-;; NEW functions with separated common parts: lf1 and lf2+
 
 (defn- lf1
   [a lvlfilter initcoll]
@@ -32,10 +40,10 @@
     (r/fold
      (r/monoid #(merge-with into %1 %2) (constantly {}))
      (fn ([] (hash-map currlist []))
-         ([rcoll b]
-           (if (lvlfilter a b)
-             (assoc rcoll currlist (conj (get rcoll a) b))
-            rcoll)))
+       ([rcoll b]
+        (if (lvlfilter a b)
+          (assoc rcoll currlist (conj (get rcoll a) b))
+          rcoll)))
      initcoll)))
 
 (defn- lf2+
@@ -43,12 +51,13 @@
   (let [currlist (conj klist last-match)];a b c
     (r/fold
      (r/monoid #(merge-with into %1 %2) (constantly {}))
-     (fn ([]  (hash-map currlist []))
-         ([rcoll currf]
-           (if (apply lvlfilter (conj currlist currf))
-             (assoc rcoll currlist (conj (get rcoll currlist) currf))
-             rcoll)))
+     (fn ([] (hash-map currlist []))
+       ([rcoll currf]
+        (if (apply lvlfilter (conj currlist currf))
+          (assoc rcoll currlist (conj (get rcoll currlist) currf))
+          rcoll)))
      initcoll)))
+
 
 ;; Fastest with grouping (pmap on grouping), very slow without grouping (no pmap).
 (defn lfx-gr
@@ -81,21 +90,45 @@
     (loop [lfcoll [] i 0];lfcoll  is a collwith accumulated results of each level
       (if (= i filtercount)
         lfcoll
-        (cond
-          (= i 0)
-          (recur (into [] (pmap (fn [fact] (lf1 fact (nth filters i) initcoll)) initcoll)) (inc i))
-          :else
+        (if (= i 0)
+          (recur (into [] (r/filter not-empty (pmap (fn [fact] (lf1 fact (nth filters i) initcoll)) initcoll))) (inc i))
           (recur
            (into []
-             (r/flatten;TODO: try r/mapcat identity like in lfx-gr
-              (pmap
-               (fn [currfacts]
-                 (let [klist (first (keys currfacts))];a b
-                   (pmap
-                    (fn [last-match];events matched in previous level
-                      (lf2+ klist last-match (nth filters i) initcoll))
-                    (get currfacts klist))))
-               lfcoll)))
+                 (r/filter
+                  not-empty
+                  (r/mapcat (fn [lvl-data-elem]
+                              (r/mapcat
+                               (fn [klist]
+                                 (pmap
+                                  (fn [last-match];events matched in previous level
+                                    (lf2+ klist last-match (nth filters i) initcoll))
+                                  (get lvl-data-elem klist)))
+                               (keys lvl-data-elem)))
+                            lfcoll)))
+           (inc i)))))))
+
+;; Fastest with grouping (pmap on grouping), very slow without grouping (no pmap).
+(defn lfx-gr
+  [initcoll & filters]
+  (let [filtercount (count filters)]
+    (loop [lfcoll [] i 0];lfcoll  is a collwith accumulated results of each level
+      (if (= i filtercount)
+        lfcoll
+        (if (= i 0)
+          (recur (into [] (r/filter not-empty (r/map (fn [fact] (lf1 fact (nth filters i) initcoll)) initcoll)) (inc i)) (inc i))
+          (recur
+           (into []
+                 (r/filter
+                  not-empty
+                  (r/mapcat (fn [lvl-data-elem]
+                              (r/mapcat
+                               (fn [klist]
+                                 (pmap
+                                  (fn [last-match];events matched in previous level
+                                    (lf2+ klist last-match (nth filters i) initcoll))
+                                  (get lvl-data-elem klist)))
+                               (keys lvl-data-elem)))
+                            lfcoll)))
            (inc i)))))))
 
 (defn lfx-lazy
@@ -104,218 +137,23 @@
     (loop [lfcoll [] i 0];lfcoll  is a collwith accumulated results of each level
       (if (= i filtercount)
         lfcoll
-        (cond
-          (= i 0)
+        (if (= i 0)
           (recur (pmap (fn [fact] (lf1 fact (nth filters i) initcoll)) initcoll) (inc i))
-          :else
           (recur
-           (mapcat identity
-            (pmap
-             (fn [currfacts]
-               (let [klist (first (keys currfacts))];a b
-                 (pmap
-                  (fn [last-match];events matched in previous level
-                    (lf2+ klist last-match (nth filters i) initcoll))
-                  (get currfacts klist))))
-             lfcoll))
+           (into []
+                 (r/filter
+                  not-empty
+                  (r/mapcat (fn [lvl-data-elem]
+                              (r/mapcat
+                               (fn [klist]
+                                 (pmap
+                                  (fn [last-match];events matched in previous level
+                                    (lf2+ klist last-match (nth filters i) initcoll))
+                                  (get lvl-data-elem klist)))
+                               (keys lvl-data-elem)))
+                            lfcoll)))
            (inc i)))))))
 
-
-
-;; NEGS
-
-(defn- lfneg2+
-  [klist last-match lvlfilter initcoll]
-  (let [currlist (conj klist last-match)];a b c
-    (r/fold
-     (r/monoid #(merge-with into %1 %2) (constantly {}))
-     (fn ([] (hash-map klist []))
-         ([rcoll currf]
-           (if (apply lvlfilter (conj currlist currf))
-             (assoc rcoll klist (conj (get rcoll klist) currf))
-             rcoll)))
-     initcoll)))
-
-(defn- lfneg1 [a b negfilter initcoll]
-  (let [currlist (vector a)]
-    (r/fold
-     (r/monoid #(merge-with into %1 %2) (constantly {}))
-     (fn ([] (hash-map currlist []))
-         ([rcoll c]
-            (if (negfilter a b c)
-              (assoc rcoll currlist (conj (get rcoll a) b))
-              rcoll)))
-     initcoll)))
-
-(defn lfx-neg-gr [initcoll & filters]
-  (let [filtercount (count filters)]
-  (loop [lfcoll [] i 0];lfcoll  is a collwith accumulated results of each level
-    (if (= i filtercount)
-      lfcoll
-      (let [no-neg? (clojure.test/function? (nth filters i))
-            pos-filter (if no-neg? (nth filters i) (first (nth filters i)))
-            neg-filter (if no-neg? nil (second (nth filters i)))
-            pos-coll (cond
-                       (= i 0)
-                       (into [] (r/map (fn [fact] (lf1 fact pos-filter initcoll)) initcoll))
-                       :else
-                       (into []
-                         (r/mapcat
-                          (fn [currfacts]
-                            (let [klist (first (keys currfacts))];a b
-                             (r/map
-                              (fn [last-match];events matched in previous level
-                                (lf2+ klist last-match pos-filter initcoll))
-                              (get currfacts klist))))
-                         lfcoll)))]
-      (if no-neg?
-        (recur pos-coll (inc i))
-        (recur
-         (clojure.set/difference
-          (set pos-coll)
-          (set
-           (if (= i 0)
-             (into []
-              (r/mapcat
-               (fn [currfacts]
-                 (let [a (first (keys currfacts))]
-                   (r/map
-                    (fn [b] (lfneg1 a b neg-filter initcoll))
-                    (get currfacts a))))
-               pos-coll));needs to operate on current coll
-             (into []
-              (r/mapcat
-               (fn [currfacts] ;(println (keys (mapcat identity currfacts)))
-                 (let [klist (first (keys currfacts))];a b
-                   (r/map
-                    (fn [last-match];events matched in previous level
-                      (lfneg2+ klist last-match neg-filter initcoll)
-                      (r/fold
-                       merge
-                       (fn ([] (hash-map klist '()))
-                           ([rcoll currf]
-                             (if (apply neg-filter (flatten (list klist last-match currf)))
-                               (assoc rcoll klist (conj (get rcoll klist) currf))
-                               rcoll)))
-                       initcoll))
-                      (get currfacts klist))))
-               pos-coll)))))
-         (inc i))))))))
-
-;TODO: lfx-neg-gr. lazy for negs?
-(defn lfx-neg-s [initcoll & filters]
-  (let [filtercount (count filters)]
-  (loop [lfcoll [] i 0];lfcoll  is a collwith accumulated results of each level
-    (if (= i filtercount)
-      lfcoll
-      (let [no-neg? (clojure.test/function? (nth filters i))
-            pos-filter (if no-neg? (nth filters i) (first (nth filters i)))
-            neg-filter (if no-neg? nil (second (nth filters i)))
-            pos-coll (cond
-                       (= i 0)
-                       (into [] (pmap (fn [fact] (lf1 fact pos-filter initcoll)) initcoll))
-                       :else
-                       (into []
-                         (r/flatten
-                          (pmap
-                           (fn [currfacts]
-                             (let [klist (first (keys currfacts))];a b
-                               (pmap
-                                (fn [last-match];events matched in previous level
-                                  (lf2+ klist last-match pos-filter initcoll))
-                                (get currfacts klist))))
-                           lfcoll))))]
-      (if no-neg?
-        (recur pos-coll (inc i))
-        (recur
-         (clojure.set/difference
-          (set pos-coll)
-          (set
-           (if (= i 0)
-             (into []
-              (r/flatten
-               (pmap
-                (fn [currfacts]
-                  (let [a (first (keys currfacts))]
-                    (pmap
-                     (fn [b] (lfneg1 a b neg-filter initcoll))
-                     (get currfacts a))))
-                pos-coll)));needs to operate on current coll
-             (into []
-              (r/flatten
-               (pmap
-                (fn [currfacts] ;(println (keys (mapcat identity currfacts)))
-                  (let [klist (first (keys currfacts))];a b
-                    (pmap
-                     (fn [last-match];events matched in previous level
-                       (lfneg2+ klist last-match neg-filter initcoll)
-                       (r/fold
-                         merge
-                         (fn ([] (hash-map klist '()))
-                             ([rcoll currf]
-                               (if (apply neg-filter (flatten (list klist last-match currf)))
-                                 (assoc rcoll klist (conj (get rcoll klist) currf))
-                                 rcoll)))
-                         initcoll))
-                     (get currfacts klist))))
-                pos-coll))))))
-         (inc i))))))))
-
-
-(defn lfx-neg-lazy [initcoll & filters]
-  (let [filtercount (count filters)]
-  (loop [lfcoll [] i 0];lfcoll  is a collwith accumulated results of each level
-    (if (= i filtercount)
-      lfcoll
-      (let [no-neg? (clojure.test/function? (nth filters i))
-            pos-filter (if no-neg? (nth filters i) (first (nth filters i)))
-            neg-filter (if no-neg? nil (second (nth filters i)))
-            pos-coll (cond
-                       (= i 0)
-                       (pmap (fn [fact] (lf1 fact pos-filter initcoll)) initcoll)
-                       :else
-                       (mapcat identity
-                         (pmap
-                          (fn [currfacts]
-                            (let [klist (first (keys currfacts))];a b
-                             (pmap
-                              (fn [last-match];events matched in previous level
-                                (lf2+ klist last-match pos-filter initcoll))
-                              (get currfacts klist))))
-                         lfcoll)))]
-      (if no-neg?
-        (recur pos-coll (inc i))
-        (recur
-         (clojure.set/difference
-          (set pos-coll)
-          (set
-           (if (= i 0)
-             (mapcat identity
-              (pmap
-               (fn [currfacts]
-                 (let [a (first (keys currfacts))]
-                   (pmap
-                    (fn [b] (lfneg1 a b neg-filter initcoll))
-                    (get currfacts a))))
-               pos-coll));needs to operate on current coll
-             (mapcat identity
-              (pmap
-               (fn [currfacts] ;(println (keys (mapcat identity currfacts)))
-                 (let [klist (first (keys currfacts))];a b
-                   (pmap
-                    (fn [last-match];events matched in previous level
-                      (lfneg2+ klist last-match neg-filter initcoll)
-                      (r/fold
-                       merge
-                       (fn ([] (hash-map klist '()))
-                           ([rcoll currf]
-                             (if (apply neg-filter (flatten (list klist last-match currf)))
-                               (assoc rcoll klist (conj (get rcoll klist) currf))
-                               rcoll)))
-                       initcoll))
-                      (get currfacts klist))))
-               pos-coll)))))
-         (inc i))))))))
 
 
 ;;; UTILS
@@ -323,3 +161,4 @@
 (defn lf-non-empty [res] (filter not-empty (map #(filter not-empty %) res)))
 (defn lf-group-by-hits-count [res] (filter #(< 0 (first %)) (group-by #(count (mapcat identity (vals %))) res))); groups results by counts without empty hits
 (defn lf-summary-by-hits [res] (map #(hash-map (first %) (count (second %))) (group-by #(count (mapcat identity (vals %))) res)))
+
